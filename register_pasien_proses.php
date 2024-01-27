d2cf8e96f88037ce506419323df8b9077987072e
jesper245 <90240039+jesper245@users.noreply.github.com>
2024-01-27 10:29:59 +0700

    Add files via upload

---
+++ register_pasien_proses.php
@@ -0,0 +1,20 @@
+<?php
+    include "conf/conn.php";
+    if($_POST){
+        $nama = $_POST['nama'];
+        $alamat = $_POST['alamat'];
+        $no_ktp = $_POST['no_ktp'];
+        $no_hp = $_POST['no_hp'];
+        $query = ("INSERT INTO pasien (nama,alamat,no_ktp,no_hp) 
+           VALUES ('".$nama."','".$alamat."','".$no_ktp."','".$no_hp."')");
+        if(!mysqli_query($conn,$query)){
+            die(mysql_error);
+        }else
+        {
+            echo '<script> alert("Data Berhasil Ditambahkan !!!"); window.location.href="../../index.php?page=login_pasien.php"</script>';
+        }
+        
+        header("Location: login_pasien.php");
+    }
+   
+?>
